39fc3967decef55a4ae5ee4c923c075384c0f283
Keryn Knight <keryn@kerynknight.com>
2017-01-21 12:17:03 +0000

    demo project can now be served by gunicorn, waitress, meinheld or wsgiref usages: gunicorn demo_project python demo_project.py --wsgi=meinheld python demo_project.py --wsgi=waitress

--- demo_project.py
+++ demo_project.py
@@ -1,6 +1,8 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
+
+import argparse
 import sys
 
 sys.dont_write_bytecode = True
@@ -21,6 +23,18 @@ except ImportError:
 
 if MISSING_DEPENDENCY:
     sys.exit(1)
+
+try:
+    from meinheld import server as meinheld_serve
+    HAS_MEINHELD = True
+except ImportError as e:
+    HAS_MEINHELD = False
+
+try:
+    from waitress import serve as waitress_serve
+    HAS_WAITRESS = True
+except ImportError as e:
+    HAS_WAITRESS = False
 
 import detour
 
@@ -90,7 +104,33 @@ application = detour.Detour(app=fallback, mounts=(
     ('/django', django_app),
 ))
 
-if __name__ == '__main__':
+def wsgi_via_meinheld(host, port, application):
+    meinheld_serve.listen((host, port))
+    print("Running using meinheld on port %d" % port)
+    meinheld_serve.run(application)
+
+def wsgi_via_waitress(host, port, application):
+    print("Running using waitress on port %d" % port)
+    waitress_serve(application, host=host, port=port)
+
+def wsgi_via_wsgiref(host, port, application):
     httpd = make_server('', 8080, application)
-    print("Running on port 8080")
+    print("Running using wsgiref port %d" % port)
     httpd.serve_forever()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Demo project for wsgi-detour')
+    parser.add_argument('-w', '--wsgi', action='store', choices=('wsgiref', 'meinheld', 'waitress'), default='wsgiref')
+    args = parser.parse_args(sys.argv[1:])
+    if args.wsgi == 'meinheld':
+        if HAS_MEINHELD:
+            wsgi_via_meinheld('', 8080, application)
+        else:
+            sys.stdout.write("Could not import requested WSGI publisher: meinheld\n")
+    elif args.wsgi == 'waitress':
+        if HAS_WAITRESS:
+            wsgi_via_waitress('', 8080, application)
+        else:
+            sys.stdout.write("Could not import requested WSGI publisher: waitress\n")
+    else:
+        wsgi_via_wsgiref('', 8080, application)
