171c60e87ca9cf5b24b2912010665a323aec7b79
Keryn Knight <keryn@kerynknight.com>
2017-01-21 11:30:15 +0000

    Include a Django application as part of the demo project, as well. So now we have 2 raw WSGIs, 1 Bottle, 1 Django and a fallback raw WSGI. That'll do?

--- demo_project.py
+++ demo_project.py
@@ -1,7 +1,8 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
-from __future__ import absolute_import
+from __future__ import absolute_import, unicode_literals
 import sys
+
 sys.dont_write_bytecode = True
 from wsgiref.util import setup_testing_defaults
 from wsgiref.simple_server import make_server
@@ -12,13 +13,20 @@ try:
 except ImportError:
     sys.stdout.write("You'll need to `pip install bottle` to run this demo\n")
     MISSING_DEPENDENCY = True
+try:
+    import django
+except ImportError:
+    sys.stdout.write("You'll need to `pip install Django` to run this demo\n")
+    MISSING_DEPENDENCY = True
 
 if MISSING_DEPENDENCY:
     sys.exit(1)
 
 import detour
 
-
+# ------------------------------------------------------------------------------
+# Some raw WSGI responses ...
+# ------------------------------------------------------------------------------
 
 def one_view(environ, start_response):
     setup_testing_defaults(environ)
@@ -31,26 +39,55 @@ def another_view(environ, start_response):
     start_response('200 OK', [('content-type', 'text/plain')])
     return ('Another view',)
 
+# ------------------------------------------------------------------------------
+# Now we set up a Bottle instance
+# ------------------------------------------------------------------------------
 bottle_app = Bottle()
-
 @bottle_app.route('/hello')
-def index():
+def bottle_hello_world():
     return "this went to Bottle"
 
 @bottle_app.route('/goodbye/<var>')
-def index(var):
-    return "%r also went to Bottle" % var
+def bottle_goodbye_cruel_world(var):
+    return "%s also went to Bottle" % var
 
+# ------------------------------------------------------------------------------
+# Now we set up a Django instance
+# ------------------------------------------------------------------------------
+from django.conf import settings
+settings.configure(
+    ROOT_URLCONF=__name__,
+    DEBUG=True,
+    ALLOWED_HOSTS=['*'],
+)
+from django.core.wsgi import get_wsgi_application
+from django.http import HttpResponse
+from django.conf.urls import url
+
+def django_hello_world(request):
+    return HttpResponse("I'm from Django!")
+urlpatterns = (
+    url('^$', django_hello_world),
+)
+django_app = get_wsgi_application()
+
+# ------------------------------------------------------------------------------
+# The WSGI App which Detour wraps over as a fallthrough...
+# ------------------------------------------------------------------------------
 
 def fallback(environ, start_response):
     setup_testing_defaults(environ)
     start_response('404 Not Found', [('content-type', 'text/plain')])
     return ('Fallback!',)
 
+# ------------------------------------------------------------------------------
+# Example Detour setup.
+# ------------------------------------------------------------------------------
 application = detour.Detour(app=fallback, mounts=(
-    ('/one_view', one_view),
-    ('/another_view', another_view),
+    ('/raw/one_view', one_view),
+    ('/raw/another_view', another_view),
     ('/bottled', bottle_app),
+    ('/django', django_app),
 ))
 
 if __name__ == '__main__':
